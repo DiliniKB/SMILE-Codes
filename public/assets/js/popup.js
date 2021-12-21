845846afaf2afdf0252d131d5dbd5d627dfefda3
DiliniKB <84224505+DiliniKB@users.noreply.github.com>
2021-12-21 21:22:05 +0530

    Add files via upload

---
+++ public/assets/js/popup.js
@@ -0,0 +1,21 @@
+
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
+const error1 = document.getElementById("error1")
+
+
+function popupError(x) {
+  error1.innerText  = x;
+  modal.style.display = "block";  
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
