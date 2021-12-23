0fa54038824bc3da96455ed311ff1ea128569787
DiliniKB <84224505+DiliniKB@users.noreply.github.com>
2021-12-23 18:01:47 +0530

    Add files via upload

--- app/controllers/singlefund.php
+++ app/controllers/singlefund.php
@@ -30,5 +30,38 @@ Class singlefund extends Controller
         $this->view("viewfund",$data);
     }
 
+    function report($table="",$id=""){
+        $data['table'] = $table."_report";
+        $data['fund_id'] = $id;
+        $data['page_title'] = "Report fund";
+        $data['user_id'] = $_SESSION['user_id'];
+        
+        if ($_POST) {
+            $data['feedback'] = $_POST;
+            $data['photos'] = $_FILES;
+            $fund = $this->loadModel("fund");
+            $results = $fund->report($data);
+            // show($data);
+        }
+        $this->view("report",$data);
+
+    }
+
+    function donate($category="",$id=""){
+        $data['page_title'] = "Donate";
+        $data['category'] = $category;
+        $data['type'] = "fund";
+        $data['table'] = $category."fund";
+        $data['page_title'] = "View ".$data['category']." ".$data['type'];
+        $data['id'] = $id;
+
+        $funds = $this->loadModel("fund");
+        $results = $funds->view_fund($data);
+        $data['fund'] = $results;
+
+        // show($data);
+        $this->view("fundpayment",$data);
+    }
+
     
 }
