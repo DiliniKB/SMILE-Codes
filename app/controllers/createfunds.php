90d042fc0405cec3d1b85a28996a3cf2182c3b21
DiliniKB <84224505+DiliniKB@users.noreply.github.com>
2021-12-24 22:48:31 +0530

    Add files via upload

--- app/controllers/createfunds.php
+++ app/controllers/createfunds.php
@@ -19,23 +19,25 @@
 
             $user = $this->loadModel("user");
 
-            for ($i=1; $i <4 ; $i++) { 
-                $member = 'member'.$i;
-                if(array_key_exists($member,$_POST)){
-                    $result= $user->search_user_by_email($_POST[$member]);
-                    $data[$member] = $result->user_ID;
-                    if(!$data[$member]){
-                        $_SESSION['error'] = "Please enter a registered email for".$member;
+            if($_POST){
+                for ($i=1; $i <4 ; $i++) { 
+                    $member = 'member'.$i;
+                    if($_POST[$member]){
+                        $result= $user->search_user_by_email($_POST[$member]);
+                        $data[$member] = $result->user_ID;
+                        if(!$data[$member]){
+                            $_SESSION['error'] = "Please enter a registered email for".$member;
+                        }
                     }
                 }
+
+                $uploader = $this->loadModel("fund");
+                $uploader->create_fund($_POST,$_FILES,$data);
+
+                show($data);
+                show($_POST);
+                show($_FILES);
             }
-
-            $uploader = $this->loadModel("fund");
-            $uploader->create_fund($_POST,$_FILES,$data);
-
-            show($data);
-            show($_POST);
-            show($_FILES);
 
             $this->view("create".$data['type'],$data);
         }
