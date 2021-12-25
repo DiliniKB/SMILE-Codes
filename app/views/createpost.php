e2f909a37f7927d49c72052adb2b5b083c609d46
DiliniKB <84224505+DiliniKB@users.noreply.github.com>
2021-12-25 12:27:52 +0530

    Add files via upload

--- app/views/createpost.php
+++ app/views/createpost.php
@@ -23,11 +23,11 @@
         }
 
     function info() {
-        document.getElementById("members").style.display = "block";
+        document.getElementById("mem").style.display = "block";
     }
 
     function closeForm() {
-        document.getElementById("members").style.display = "none";
+        document.getElementById("mem").style.display = "none";
     }
 </script> 
         
@@ -43,12 +43,42 @@
 
     <div id="form">
         <div class="row">    
-            <form method="post" style="margin:0px;" class="column" enctype="multipart/form-data"> 
+        <form method="post" style="margin:0px;" class="column" enctype="multipart/form-data"> 
                 <input class="input_form" type="text" placeholder="Keywords" name="keywords" required>
                 <input class="input_form" type="text" placeholder="Town" name="town" required>
-                <input class="input_form" type="text" placeholder="District" name="District" required>
+                <div>
+                    <label for="District" name="District" required></label>
+                    <select class="input_form" name="District" id="menu4" placeholder="District" required>
+                        <option value="none">Choose Your District</option>
+                        <option value="Ampara">Ampara</option>
+                        <option value="Anuradhapura">Anuradhapura</option>
+                        <option value="Badulla">Badulla</option>
+                        <option value="Batticaloa">Batticaloa</option>
+                        <option value="Colombo">Colombo</option>
+                        <option value="Galle">Galle</option>
+                        <option value="Gampaha">Gampaha</option>
+                        <option value="Hambantota">Hambantota</option>
+                        <option value="Jaffna">Jaffna</option>
+                        <option value="Kalutara">Kalutara</option>
+                        <option value="Kandy">Kandy</option>
+                        <option value="Kegalle">Kegalle</option>
+                        <option value="Kilinochchi">Kilinochchi</option>
+                        <option value="Kurunegala">Kurunegala</option>
+                        <option value="Mannar">Mannar</option>
+                        <option value="Matale">Matale</option>
+                        <option value="Matara">Matara</option>
+                        <option value="Moneragala">Moneragala</option>
+                        <option value="Mullaitivu">Mullaitivu</option>
+                        <option value="Nuwara_Eliya">Nuwara Eliya</option>
+                        <option value="Polonnaruwa">Polonnaruwa</option>
+                        <option value="Puttalam">Puttalam</option>
+                        <option value="Ratnapura">Ratnapura</option>
+                        <option value="Trincomalee">Trincomalee</option>
+                        <option value="Vavuniya">Vavuniya</option>
+                    </select>
+                </div>
                 <input class="input_form" type="text" placeholder="Item" name="Item" required>
-                <input class="input_form" type="text" placeholder="Description" name="description" required>
+                <input class="input_form" type="text" placeholder="Description" name="description" maxlength="150" required>
 
                 <div>
                     <label for="type" name="type" required></label>
@@ -58,11 +88,12 @@
                     </select>
                 </div>
 
-                <div style="display: inline;">
-                    <input class="input_radio" type="radio" name="usertype" required>
-                    <label class="RB">Individual</label>
+                <div style="display: inline; left:50%;">
                     <input class="input_radio" type="radio" onclick="info()" name="usertype" required>
                     <label class="RB">Team</label>
+                    <input class="input_radio" type="radio" name="usertype"  onclick="closeForm()" required>
+                    <label class="RB">Individual</label>
+                    
                 </div>
 
                 <div class="input_form" id="anon" >
@@ -72,25 +103,21 @@
                 <button id="enter" type="submit" name="create" >ENTER</button>        
             
            
-            <div class="column">
-                <div class="uploadBox">
+                <div class="column">
+                    <div class="uploadBox">
                     <img id="output_image"/>
+                    </div>
+                    <input type="file" class="custom-file-input" onchange="preview_image(event)" name="file" >                    
                 </div>
-                <input type="file" class="custom-file-input" onchange="preview_image(event)" name="file" >                    
-            </div>
-        </div>         
-    </div>  
-    
-    <form id="members">
-        <div class="field" id="mem">
-            <input class="member" type="text" placeholder="Member1 email" name="member1" >
-            <input class="member" type="text" placeholder="Member2 email" name="member2" >
-            <input class="member" type="text" placeholder="Member3 email" name="member3" >
-        </div>
-        <button id="enter" type="submit" onclick="closeForm()">DONE</button>
-    </form>  
-    
-    </form>
+
+                <!--<form method="post" id="members">-->
+                <div class="field" id="mem">
+                        <input class="member" type="email" placeholder="Member1 email" name="member1" >
+                        <input class="member" type="email" placeholder="Member2 email" name="member2" >
+                        <input class="member" type="email" placeholder="Member3 email" name="member3" >
+                </div>
+                <!---<button id="enter" type="submit" onclick="closeForm()" name="memberAdd">DONE</button>--->
+            </form>
 
 </body>   
 </html>
