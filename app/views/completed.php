2ec2cf56e17ab87296c32f2472d92ae7eedf1d3c
DiliniKB <2019cs010@stu.ucsc.lk>
2021-12-30 12:27:36 +0530

    Verification UIs

---
+++ app/views/completed.php
@@ -0,0 +1,22 @@
+<?php $this->view("footers/footerMain",$data);?>
+<?php $this->view("header",$data);?>
+<head>
+    <link rel="stylesheet" href="<?=ASSETS?>css/stylesfooter.css">
+    <link rel="stylesheet" href="<?=ASSETS?>css/styles.css">
+    <link rel="stylesheet" href="<?=ASSETS?>css/stylesBigheader.css">
+    <script type="text/javascript" src="<?=ASSETS?>js/index.js"></script>
+</head>
+<body>
+    <div class="BlurRectOval" id="quote">
+        <h3>Identity information is submitted.</h3>
+        Your account is in pending state now. <br />Once one of administrators approved <br /> your account, you can enjoy the <br />full features of the platform.
+        <br />
+        <br />
+        <a href="dashboard">Continue</a>
+    </div>
+                   
+    <img src="<?=ASSETS?>images/mainPages/1asset_3_1.png" id="image">
+    <img src="<?=ASSETS?>images/mainPages/random lines.png" id="randomLines">
+    
+</body>
+</html>
---
+++ app/views/invalid-link.php
@@ -0,0 +1,22 @@
+<?php $this->view("footers/footerMain",$data);?>
+<?php $this->view("header",$data);?>
+<head>
+    <link rel="stylesheet" href="<?=ASSETS?>css/stylesfooter.css">
+    <link rel="stylesheet" href="<?=ASSETS?>css/styles.css">
+    <link rel="stylesheet" href="<?=ASSETS?>css/stylesBigheader.css">
+    <script type="text/javascript" src="<?=ASSETS?>js/index.js"></script>
+</head>
+<body>
+    <div class="BlurRectOval" id="quote">
+        <h3>Invalid or expired link</h3>
+        Please try again and contact us
+        <br />
+        <br />
+        <a href="../../resendemail">Resend</a>
+    </div>
+                   
+    <img src="<?=ASSETS?>images/mainPages/1asset_3_1.png" id="image">
+    <img src="<?=ASSETS?>images/mainPages/random lines.png" id="randomLines">
+    
+</body>
+</html>
